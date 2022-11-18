6f2aaf99ecf1b1d62fe942c969cf3721cdcc2994
desonglll <lindesong666@163.com>
2022-11-18 12:28:01 +0800

    2022-11-18 12:28:01

--- vim-plug/vim-plug-install.sh
+++ vim-plug/vim-plug-install.sh
@@ -1,2 +1,4 @@
+mkdir "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim
+mkdir "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site
 mkdir "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
 cp ./vim-plug.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
